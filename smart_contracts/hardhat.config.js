d785d41aae6d7992c7ca8b3c359fe6a4a08c7cc5
Yash Garg <yashygkf@gmail.com>
2023-06-10 15:45:41 +0530

    edit: change the solidity version to 0.8.19

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.19",
   network: {
     mumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
