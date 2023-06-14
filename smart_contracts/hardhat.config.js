700e0ad6d12c226f527c806483a747a32a544407
Yash Garg <yashygkf@gmail.com>
2023-06-14 17:27:11 +0530

    feat: configured hardhat env

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,11 +1,29 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ALCHEMY_HTTP_URL = process.env.ALCHEMY_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const POLYGON_SCAN_KEY = process.env.POLYGON_SCAN_KEY;
+
 module.exports = {
-  solidity: "0.8.19",
-  network: {
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
-  }
+  },
+  networks: {
+    mumbai: {
+      url: ALCHEMY_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: POLYGON_SCAN_KEY,
+    },
+  },
 };
